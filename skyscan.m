094bb1060549d4c11a1689677121538ac6933dac
Andrea Lanterna <a.lanterna@campus.unimib.it>
2019-04-24 23:42:02 +0200

    Adds integral support

--- skyscan.m
+++ skyscan.m
@@ -7,18 +7,22 @@ function  [out] = skyscan(in)
 %
 %   skyscan(in) runs the program with options in the "in" file
 %
-
+%   out = skyscan(in) returns trapz integrals on the rows
+%
 narginchk(0,1)
 
 %% set defaults
 
+%filesystem defaults
 dflt.recur_over_folder=true;
 dflt.filename='';
+dflt.custom_directory='';
+
+%graphic defaults
 dflt.make_plot=true;
 dflt.sample_module=1;
 dflt.dedicated_figure_per_file=true;     %if false, one plot for all files
 dflt.enable_browser=false;
-dflt.custom_directory='';
 
 %% input handling and checks
 
@@ -34,40 +38,54 @@ for fname = fieldnames(dflt)
     end
 end
 
-% fill short-named variables + consistency checks
+% fill short-named variables
 
-% file/directory reading
+% filesystem reading
 flist=[in.filename,""];                 % I need it to be an array
 recr=in.recur_over_folder;
 cudir=in.custom_directory;
 
-% graphics
+% graphics reading
 plot=in.make_plot;
 dfpf=in.dedicated_figure_per_file;
 brws=in.enable_browser;
+smpl=in.sample_module;
 
-% sample ratio: as it can break the function if ill defined, the function
-% will always redefine it as a positive power of 2 guaranteed to be 
-% well behaved
+%% Consistency checks
 
-if in.sample_module <= 0
-    smpl=1;
-else
-    smpl=pow2(floor(log2(in.sample_module)));
-end
-
-fprintf('The sample ratio has been rounded to the nearest smallest positive n^2 = %d\n',smpl);
-
-if smpl>8192
-   fprintf('The given sampling ratio is bigger than the data set: it will be changed to 512');
-   smpl=512;
-end
-
-% Checks about recursion and file name
+% Filesystem
 
 if ~recr && flist(1)==("")
-    error("If you don't want to recur over a folder, you must specify a filename");
+    error("If you don't want to recur over a directory, you must specify a filename");
     return;
+end
+
+if recr && flist(1)~=("")
+    warning("As you have specified a filename, recur will be set to false");
+    recr=false;
+end    
+
+% Graphics (useful only if plot needed)
+
+if plot 
+
+    % sample ratio: as it can break the function if ill defined, the
+    % function will always redefine it as a positive power of 2 guaranteed
+    % to be well behaved
+
+    if smpl <= 0
+        smpl=1;
+    else
+        smpl=pow2(floor(log2(smpl)));
+    end
+
+    fprintf('The sample ratio has been rounded to the nearest smallest positive n^2 = %d\n',smpl);
+
+    if smpl>8192
+       disp('The given sampling ratio is bigger than the data set: it will be changed to 512');
+       smpl=512;
+    end
+
 end
 
 %% text files handling
@@ -75,15 +93,11 @@ end
 if recr % Working on a directory
     if isempty(cudir)
         [cudir,~,~]=fileparts(mfilename('fullpath'));
+        disp("You don't have specified a custom directory");
     end
     cd(cudir);
     fprintf('All the data files in %s will be analyzed\n', cudir);
-    try
-        filefinder=dir('*_USRP.txt');
-    catch
-        disp("dir() has not found *_USRP.txt files in the directory");
-        return;
-    end
+    filefinder=dir('*_USRP.txt');
     flist=[filefinder.name,""];         %Weird workaround
 end
 
@@ -99,6 +113,9 @@ end
 % header=data(:,1:3,:); %Just in case they can prove useful
 data(:,1:3,:)=[]; %Clean unwanted data
 
+rows=size(data,1);
+cols=size(data,2);
+
 %At this point the function has loaded all the y data in a 3D matrix (2D if
 %single file mode). It's faster than a cell but requires the memory
 %allocated to be contiguos so it's probabily a bad idea to use with a lot
@@ -109,15 +126,21 @@ data(:,1:3,:)=[]; %Clean unwanted data
 %% Managing X
 % As provided by the lab guy, just copy-pasted.
 
-x = 1:size(data,2);
+x = 1:cols;
 x = x*19531;
 x = x + 1300001024;
 x = (x - 19531)';
 
 %% Integral time
 
-% To be written
-
+integral = zeros(rows,nfiles);
+for k=1:nfiles
+    for n=1:rows
+        integral(n,k)=trapz(data(n,:));
+    end
+end
+out = integral;
+    
 %% Plot time
 
 if plot
@@ -129,10 +152,8 @@ if plot
     
     % This section shows some quite bad examples of using MATLAB. Be aware!
     
-    sz=size(data,1);
-    
     if dfpf
-        cmap=jet(sz);
+        cmap=jet(rows);
         colorpicker=@dedi_picker;
         fig_lim=inf;
     else
@@ -152,14 +173,12 @@ if plot
             figure('Name',flist(c));
         end
         hold on
-        for k=1:sz
+        for k=1:rows
             y=data(k,:,c);
             scatter(x,y,1,colorpicker(cmap,k,c));
         end
         hold off
-        if c==1 || c==nfiles
-            is_brws(sz,flist,c);
-        end
+        is_brws(rows,flist,c);
     end
   
     if fig_lim==1
@@ -170,12 +189,14 @@ end
 
 function l=legendgenerator(sz,flist,c)
 % LEGENDGENERATOR is useful for giving names in plotbrowser
+
 flist=regexprep(flist, '_USRP.txt', '', 'lineanchors');
 nmb=(1:sz)';
 l=[];
 for k=1:c
     str=repmat(flist(k),[sz 1]);
     l=[l,strcat(str, {'  Line:'}, num2str(nmb))];
+    %Preallocating would be better, but nevermind.
 end
 
 function plotlegend(sz,flist,c)
@@ -183,7 +204,7 @@ legend(legendgenerator(sz,flist,c));
 plotbrowser;
 legend('toggle')
 
-function nothing(sz,flist,c)
+function nothing(~,~,~)
 return;
 
 function color=dedi_picker(cmap,k,~)
