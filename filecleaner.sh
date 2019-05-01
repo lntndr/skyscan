9b9bf2316347d5ce3869012c022533ef358f39f4
Andrea Lanterna <a.lanterna@campus.unimib.it>
2019-05-01 21:49:41 +0200

    Fixes linux filecleaner

--- filecleaner.sh
+++ filecleaner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-find . -type f -name "*_USRP.txt" | xargs sed -i'/^\s*$/d'
-find . -type f -name "*_USRP.txt" | xargs sed -i"s/,/./g"
-find . -type f -name "*_USRP.txt" | xargs sed -i"s/[[:space:]]\+/,/g"
+find . -type f -name "*_USRP.txt" | xargs sed -i '/^\s*$/d'
+find . -type f -name "*_USRP.txt" | xargs sed -i "s/,/./g"
+find . -type f -name "*_USRP.txt" | xargs sed -i "s/[[:space:]]\+/,/g"
